fc085f9d1a41eda38c5798de956a82436bc644a5
Andrew Hwang <Andrew@Andrews-MBP.attlocal.net>
2018-04-23 14:41:53 -0700

    Andrews commit

--- user-api/routes/users.js
+++ user-api/routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var userController = require('../controllers/user-controller');
-var authenticate = require('../controllers/auth');
+var auth = require('../controllers/auth');
 
-router.get('/', authenticate.isAuthenticated, userController.index);
-router.get('/:id', userController.getById);
-router.post('/', userController.create);
+router.get('/', auth.verifyToken, userController.index);
+router.get('/:id', auth.verifyToken, userController.getById);
+router.post('/', auth.verifyToken, userController.create);
 router.post('/login', userController.login);
 
 module.exports = router;
