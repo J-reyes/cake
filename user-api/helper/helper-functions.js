3aa030cb538a250f277cd4cdf3cca8717b9e796b
Jose Vazquez Reyes <lalovazquez@Joses-MBP.attlocal.net>
2018-04-24 18:26:13 -0700

    filter data for fraud

--- user-api/helper/helper-functions.js
+++ user-api/helper/helper-functions.js
@@ -1,5 +1,26 @@
 const request = require('request');
 
+
+// returns an object of objects with information about transactions and fraud per day
+
+const filterDateData = (start, end, data) => {
+    let dates = buildDateArray(start, end);
+    let returnObj = {}
+
+    dates.forEach(day => {
+        let dayString = day.toLocaleString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
+        returnObj[dayString] = {
+            transactions: getTransactionsOnDay(day, data),
+            fraudRatio: getFraudRatioOnDay(day, data)
+        }
+    });
+
+    return returnObj;
+}
+
+
+
+
 // Request Library Promise Wrapper
 const requestPromise = options => new Promise((resolve, reject) => {
     request(options, (err, res, body) => {
@@ -11,4 +32,61 @@ const requestPromise = options => new Promise((resolve, reject) => {
     })
 });
 
-module.exports = { requestPromise };
+// ****************** filterDateData Helper Functions ******************
+
+// date is a Date obj
+const compareDates = (date, resObj) => {
+    const objDate = new Date(resObj.timeStamp);
+
+    // check if dates are the same
+    if (isSameDay(objDate, date)) {
+        return true;
+    }
+    return false;
+}
+
+// get array of transactions on same day
+const getTransactionsOnDay = (date, transactions) => transactions.filter(t => compareDates(date, t));
+
+const getFraudRatioOnDay = (date, transactions) => {
+    const transactionsOnDate = getTransactionsOnDay(date, transactions);
+    const amtTransactions = transactionsOnDate.length;
+    const amtFraud = transactionsOnDate.filter(t => t.fraud > -1.5).length;
+
+
+    // avoid division by zero
+    if (amtFraud !== 0) {
+        return amtTransactions / amtFraud;
+    }
+    // zero fraud
+    return 0;
+}
+
+// builds an array of dates between two dates (inclusive)
+// must be same year!
+// [date(4/1/2018), date(4/2/2018)]
+
+const buildDateArray = (start, end) => {
+    let arr = [];
+    currDate = start;
+    while (!isSameDay(currDate, end)) {
+        arr.push(new Date(currDate));
+
+        // increment date
+        currDate.setDate(currDate.getDate() + 1);
+    }
+    // includes end date
+    arr.push(currDate);
+    return arr;
+}
+
+
+// compares date objects based on day, month, year
+const isSameDay = (day0, day1) =>
+    day0.getDate() === day1.getDate() &&
+    day0.getMonth() === day1.getMonth() &&
+    day0.getFullYear() === day1.getFullYear();
+
+// ********************************************************
+
+module.exports = { requestPromise, filterDateData };
